00b6f936348f26a22b3b1aae57c05e35937ada1b
ndip007 <owenhojunior@gmail.com>
2018-05-18 19:54:56 +0100

    Updating references of Readme file

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ setup_dict = dict(
     maintainer_email=metadata.emails[0],
     url=metadata.url,
     description=metadata.description,
-    long_description=read('README.rst'),
+    long_description=read('README.md'),
     # Find a list of classifiers here:
     # <http://pypi.python.org/pypi?%3Aaction=list_classifiers>
     classifiers=[

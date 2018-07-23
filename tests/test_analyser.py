47a4ae0a14b1e7b9c775dad01931d51f721fec6a
ndip007 <owenhojunior@gmail.com>
2018-07-23 16:36:24 +0100

    Committing tests v2

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -22,9 +22,9 @@ class TestAnalyser(object):
     def test_pip_analyser(self):
         python_project_path = normalize_path(os.path.join(TEST_DIR, 'example/py_project'))
         project_info = determine_build_tool(normalize_path(python_project_path))
-        package_json_path = normalize_file_path(os.path.join(TEST_DIR, 'example/py_project/requirements.txt'))
+        requirements_file_path = normalize_file_path(os.path.join(TEST_DIR, 'example/py_project/requirements.txt'))
         assert "pip" in project_info[0]
         pkg_mgr = PipPackageManager(normalize_path(python_project_path), True)
         assert pkg_mgr.project_dir == normalize_path(os.path.join(TEST_DIR, 'example/py_project'))
         assert pkg_mgr.in_tests == True
-        assert pkg_mgr.files_to_parse == [[package_json_path]]
+        assert pkg_mgr.files_to_parse == [[requirements_file_path]]

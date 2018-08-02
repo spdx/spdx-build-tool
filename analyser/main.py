305aff01db3b311c6a123b882a4532bc21b84168
ndip007 <owenhojunior@gmail.com>
2018-08-02 12:55:55 +0100

    Update main.py

--- analyser/main.py
+++ analyser/main.py
@@ -32,39 +32,39 @@ def main(argv):
     :type argv: :class:`list`
     """
     # First argument must be the project directory
-    # print("argv", argv)
+    print("argv", argv)
     return analyse_dir(True, argv[1])
-    epilog = '''
-            {project} {version}
-
-            {authors}
-            URL: <{url}>
-            '''.format(
-        project=setup_dict['name'],
-        version=setup_dict['version'],
-        authors=setup_dict['author'],
-        url=setup_dict['url'])
-    arg_parser = argparse.ArgumentParser(
-        prog=argv[0],
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=setup_dict['description'],
-        epilog=epilog)
-    arg_parser.add_argument(
-        '-V', '--version',
-        action='version',
-        help='Display the project\'s version',
-        version='{0} {1}'.format(setup_dict['name'], setup_dict['version']))
-    arg_parser.add_argument(
-        '-I',
-        '--info',
-        action='version',
-        help='Display the project\'s readme content',
-        version='{0} {1}'.format(
-            setup_dict['name'],
-            setup_dict['long_description']))
-
-    arg_parser.parse_args(args=argv[1:])
-    return analyse_dir(True, argv[1])
+    # epilog = '''
+    #         {project} {version}
+    #
+    #         {authors}
+    #         URL: <{url}>
+    #         '''.format(
+    #     project=setup_dict['name'],
+    #     version=setup_dict['version'],
+    #     authors=setup_dict['author'],
+    #     url=setup_dict['url'])
+    # arg_parser = argparse.ArgumentParser(
+    #     prog=argv[0],
+    #     formatter_class=argparse.RawDescriptionHelpFormatter,
+    #     description=setup_dict['description'],
+    #     epilog=epilog)
+    # arg_parser.add_argument(
+    #     '-V', '--version',
+    #     action='version',
+    #     help='Display the project\'s version',
+    #     version='{0} {1}'.format(setup_dict['name'], setup_dict['version']))
+    # arg_parser.add_argument(
+    #     '-I',
+    #     '--info',
+    #     action='version',
+    #     help='Display the project\'s readme content',
+    #     version='{0} {1}'.format(
+    #         setup_dict['name'],
+    #         setup_dict['long_description']))
+    #
+    # arg_parser.parse_args(args=argv[1:])
+    # return analyse_dir(True, argv[1])
     # pass
 
 
